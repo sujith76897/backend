804ca305bf86e284549b458de3f0c6f84e501652
sujith76897 <sujith76897@gmail.com>
2025-03-07 14:40:58 +0530

    add

--- server.js
+++ server.js
@@ -110,11 +110,3 @@ app.post('/api/login', async (req, res) => {
     }
 });
 
-// Serve static files
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
