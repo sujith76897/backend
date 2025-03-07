adfb8c6965da5b4807c1dd146e2edb1e2d4c5bde
sujith76897 <sujith76897@gmail.com>
2025-03-07 15:14:18 +0530

    add

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // MongoDB Connection with better error handling
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI,)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => {
     console.error('MongoDB connection error:', err);
